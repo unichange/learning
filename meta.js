44e5d254e88e152d3fae377770cd1ef469e09c1d
unichange <167487503+unichange@users.noreply.github.com>
2024-04-19 10:34:00 +0930

    Create meta.js

---
+++ meta.js
@@ -0,0 +1,7 @@
+{
+  "name": "Aussie Mate",
+  "website": "https://twitter.com/aussiemate",
+  "description": "Aussie girl and aussie mate!",
+  "logo": "https://raw.githubusercontent.com/unichange/learning/main/pig_tai.png",
+  "twitter": "https://twitter.com/aussiemate"
+}
